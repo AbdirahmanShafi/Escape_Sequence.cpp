1f49aef78a6ba8001182aae9b1e5732ff54cfdf9
AbdirahmanShafi <nemibase2@gmail.com>
2022-10-13 18:01:48 +0300

    git@github.com:AbdirahmanShafi/Escape_Sequence.cpp.git

---
+++ Escape_Sequence.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<" \\\\\t\t\t////\\\\\n/‾‾‾‾‾\\\t\t\t/‾‾‾‾‾\\\n|‾  ‾ |\t\t\t|‾  ‾ |\n \\__/\t\t\t \\__/\n\\_____/\t\t\t\\_____/";
+    cout<<" \n   |\t\t\t   |\n   |\t\t\t   |\n   |\t\t\t  // \\ \\\n  //|\\\t\t\t //   \\ \\\n / | \\\t\t\t/     \\ \\\n/  |  \\\t\t\t/      \\ \\\n   |   |\t\t--------  _\n   |\t\t\t|  |\n   /\\\t\t\t‾‾  ‾‾\n  /  \\\n /    \\";
+    
+    
+}
